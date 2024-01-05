ac4425b529bc123163435d156d8e40766beed113
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-05 11:31:36 +0700

    Presence App | add presence location

---
+++ admin/data_presence_locations/addPresenceLocations.php
@@ -0,0 +1,147 @@
+<?php 
+
+session_start();
+ob_start();
+
+if (!isset($_SESSION['login'])) {
+  header('Location: ../../auth/login.php?message=not_login_yet');
+} else  if ($_SESSION['role'] != 'admin') {
+  header('Location: ../../auth/login.php?message=deny_access');
+}
+
+$title = "Add Presence Location";
+
+include ('../layout/header.php');
+
+require_once('../../config.php');
+
+if (isset($_POST['save'])) {
+  $name_location = htmlspecialchars($_POST['name_location']);
+  $address_location = htmlspecialchars($_POST['address_location']);
+  $type_location = htmlspecialchars($_POST['type_location']);
+  $latitude = htmlspecialchars($_POST['latitude']);
+  $longitude = htmlspecialchars($_POST['longitude']);
+  $radius = htmlspecialchars($_POST['radius']);
+  $time_zone = htmlspecialchars($_POST['time_zone']);
+  $entry_time = htmlspecialchars($_POST['entry_time']);
+  $out_time = htmlspecialchars($_POST['out_time']);
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($name_location)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Name loc.n is mandatory!";
+    }
+    if (empty($address_location)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Address loc. is mandatory!";
+    }
+    if (empty($type_location)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Type loc. is mandatory!";
+    }
+    if (empty($latitude)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Latitude is mandatory!";
+    }
+    if (empty($longitude)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Longitude is mandatory!";
+    }
+    if (empty($radius)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Radius is mandatory!";
+    }
+    if (empty($time_zone)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Time Zone is mandatory!";
+    }
+    if (empty($entry_time)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Entry Time is mandatory!";
+    }
+    if (empty($out_time)) {
+      $error_message[] = "<i class='fa-solid fa-check'></i>Out Time is mandatory!";
+    }
+
+    if (!empty($error_message)) {
+      $_SESSION['validation'] = implode("<br>", $error_message); // ubah array menjadi string
+    } else {
+      $result = mysqli_query($connect, "INSERT INTO presence_location(name_location, address_location, 
+      type_location, latitude, longitude, radius, time_zone, entry_time, out_time) VALUES 
+      ('$name_location', '$address_location', '$type_location', '$latitude', '$longitude','$radius',
+      '$time_zone','$entry_time','$out_time')");
+
+      $_SESSION['succeed'] = 'Data saved successfully';
+      header("Location: presenceLocations.php");
+      exit;
+    }
+  }
+}
+
+// $result = mysqli_query($connect, "SELECT * FROM presence_location ORDER BY id DESC");
+?>
+
+<div class="page-body">
+  <div class="container-xl">
+
+  <div class="card col-md-6">
+    <div class="card-body">
+        <form action="<?= base_url('admin/data_presence_locations/addPresenceLocations.php') ?>" method="POST">
+            <div class="mb-3">
+              <label for="">Name Location</label>
+              <input type="text" class="form-control" name="name_location">
+            </div>
+
+            <div class="mb-3">
+              <label for="">Address Location</label>
+              <input type="text" class="form-control" name="address_location">
+            </div>
+
+            <div class="mb-3">
+              <label for="">Type Location</label>
+              <select class="form-control" name="type_location">
+                <option value="">--Choose Type Location--</option>
+                <option value="Center">Center</option>
+                <option value="Branch">Branch</option>
+              </select> 
+            </div>
+
+            <div class="mb-3">
+              <label for="">Latitude</label>
+              <input type="text" class="form-control" name="latitude">
+            </div>
+
+            <div class="mb-3">
+              <label for="">Longitude</label>
+              <input type="text" class="form-control" name="longitude">
+            </div>
+
+            <div class="mb-3">
+              <label for="">Radius</label>
+              <input type="number" class="form-control" name="radius">
+            </div>
+            
+            <div class="mb-3">
+              <label for="">Time Zone</label>
+              <select class="form-control" name="time_zone">
+                <option value="">--Choose Type Location--</option>
+                <option value="WIB">WIB</option>
+                <option value="WITA">WITA</option>
+                <option value="WIT">WIT</option>
+              </select> 
+            </div>
+
+            <div class="mb-3">
+              <label for="">Entry Time</label>
+              <input type="time" class="form-control" name="entry_time">
+            </div>
+            
+            <div class="mb-3">
+              <label for="">Out Time</label>
+              <input type="time" class="form-control" name="out_time">
+            </div>
+
+            <button type="submit" class="btn btn-primary" name="save">Save</button>
+
+        </form>
+
+  </div>
+
+  </div>
+</div>
+
+<?php 
+include ('../layout/footer.php')
+?>
---
+++ admin/data_presence_locations/presenceLocations.php
@@ -0,0 +1,65 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header('Location: ../../auth/login.php?message=not_login_yet');
+} else  if ($_SESSION['role'] != 'admin') {
+  header('Location: ../../auth/login.php?message=deny_access');
+}
+
+$title = "Data Presence Location";
+
+include ('../layout/header.php');
+
+require_once('../../config.php');
+
+$result = mysqli_query($connect, "SELECT * FROM presence_location ORDER BY id DESC");
+?>
+
+
+
+<div class="page-body">
+  <div class="container-xl">
+
+  <a href="<?= base_url('admin/data_presence_locations/addPresenceLocations.php') ?>" class="btn btn-primary"><span class="text"><i class="fa-solid fa-circle-plus"></i> Add Data</span></a>
+
+  <table class="table table-bordered mt-3">
+    <tr class="text-center">
+      <th>No</th>
+      <th>Name Location</th>
+      <th>Type Location</th>
+      <th>Latitude/Longitude</th>
+      <th>Radius</th>
+      <th>Action</th>
+    </tr>
+
+    <?php if(mysqli_num_rows($result) === 0) { ?>
+      <tr>
+        <td colspan="6">Empty data, please add new data!</td>
+      </tr>
+      <?php }else{ ?>
+        <?php $no=1; while($location = mysqli_fetch_array($result)) : ?>
+          <tr>
+            <td><?= $no++ ?></td>
+            <td><?= $location['name_location'] ?></td>
+            <td><?= $location['address_location'] ?></td>
+            <td><?= $location['latitude'] . '/' . $location['longitude'] ?></td>
+            <td><?= $location['radius'] ?></td>
+            <td class="text-center">
+              <a href="<?= base_url('admin/data_presence_locations/detailPresenceLocation.php/' . $location['id']) ?>" class="badge badge-pill bg-primary">Detail</a>
+              
+              <a href="<?= base_url('admin/data_presence_locations/editPresenceLocation.php/' . $location['id']) ?>" class="badge badge-pill bg-primary">Edit</a>
+              
+              <a href="<?= base_url('admin/data_presence_locations/deletePresenceLocation.php/' . $location['id']) ?>" class="badge badge-pill bg-danger">Delete</a>
+            </td>
+          </tr>
+        <?php endwhile; ?>  
+      <?php } ?>
+  </table>
+  </div>
+</div>
+
+<?php 
+include ('../layout/footer.php')
+?>
